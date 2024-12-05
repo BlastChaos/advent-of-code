3f32e0650e069705ea054a866002b5033f0dd022
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-12-04 21:08:01 -0500

    Day 4 question 2

--- Day 4/response.py
+++ Day 4/response.py
@@ -4,6 +4,7 @@ inputPath = "./Day 4/input.txt"
 file = open(inputPath, "r")
 
 wordPermitted = ['X', 'M', 'A', 'S']
+wordPermittedV2 = ['M', 'A', 'S']
 
 values = []
 
@@ -19,6 +20,22 @@ def isPermitted(incrementX, incrementY, values, x, y):
         if(values[positionX][positionY] != wordPermitted[i]):
             return False
     return True
+
+
+def isPermittedV2(values, x, y):
+    if(values[x][y] != 'A'):
+        return False
+    
+
+    if(x-1 < 0 or y-1 < 0):
+        return False
+    
+    if(x+1 >= len(values) or y+1 >= len(values[x])):
+        return False
+    
+    cross1 = (values[x-1][y-1] == 'M' and values[x+1][y+1] == 'S') or (values[x-1][y-1] == 'S' and values[x+1][y+1] == 'M')
+    cross2 = (values[x-1][y+1] == 'M' and values[x+1][y-1] == 'S') or (values[x-1][y+1] == 'S' and values[x+1][y-1] == 'M')
+    return cross1 and cross2
 
 
 for line in file:
@@ -37,21 +54,7 @@ count = 0
 
 for i in range(len(values)):
     for j in range(len(values[0])):
-        if(isPermitted(-1, -1, values, i, j)):
-            count+=1
-        if(isPermitted(1, 1, values, i, j)):
-            count+=1
-        if(isPermitted(1, 0, values, i, j)):
-            count+=1
-        if(isPermitted(-1, 0, values, i, j)):
-            count+=1
-        if(isPermitted(0, 1, values, i, j)):
-            count+=1
-        if(isPermitted(0, -1, values, i, j)):
-            count+=1
-        if(isPermitted(1, -1, values, i, j)):
-            count+=1
-        if(isPermitted(-1, 1, values, i, j)):
+        if(isPermittedV2(values, i, j)):
             count+=1
     
 
