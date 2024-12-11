d962c365d2ad66a2f2dbd6cf3e530318b5ba1299
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-12-10 20:18:08 -0500

    day 2

--- Day 10/response.py
+++ Day 10/response.py
@@ -20,13 +20,11 @@ for i in range(len(contents)):
     position.append(positionRow)
 
 
-def findPath(x, y, number, positionAlreadyVisited = set()):
+
+
+def findPath(x, y, number):
     if number == 9:
-        if (x,y) in positionAlreadyVisited:
-            return 0
-        else:
-            positionAlreadyVisited.add((x,y))
-            return 1
+        return 1
     
 
     pathResult = 0
@@ -34,24 +32,65 @@ def findPath(x, y, number, positionAlreadyVisited = set()):
     numberIncreased = number + 1
 
     if x >0 and position[x-1][y] == numberIncreased:
-        pathResult += findPath(x-1, y, numberIncreased, positionAlreadyVisited)
+        pathResult += findPath(x-1, y, numberIncreased)
     
     if x < len(position) - 1 and position[x+1][y] == numberIncreased:
-        pathResult+= findPath(x+1, y, numberIncreased, positionAlreadyVisited)
+        pathResult+= findPath(x+1, y, numberIncreased)
     
     if y > 0 and position[x][y-1] == numberIncreased:
-        pathResult+= findPath(x, y-1, numberIncreased, positionAlreadyVisited)
+        pathResult+= findPath(x, y-1, numberIncreased)
     
     if y < len(position[0]) - 1 and position[x][y+1] == numberIncreased:
-        pathResult+= findPath(x, y+1, numberIncreased, positionAlreadyVisited)
+        pathResult+= findPath(x, y+1, numberIncreased)
     
     return pathResult
 
 result = 0
 for x,y in startPosition:
-    result = result + findPath(x, y, 0, set())
+    result = result + findPath(x, y, 0)
 
 print(result)
+
+
+
+
+
+
+
+
+#Part 1
+# def findPath(x, y, number, positionAlreadyVisited = set()):
+#     if number == 9:
+#         if (x,y) in positionAlreadyVisited:
+#             return 0
+#         else:
+#             positionAlreadyVisited.add((x,y))
+#             return 1
+    
+
+#     pathResult = 0
+
+#     numberIncreased = number + 1
+
+#     if x >0 and position[x-1][y] == numberIncreased:
+#         pathResult += findPath(x-1, y, numberIncreased, positionAlreadyVisited)
+    
+#     if x < len(position) - 1 and position[x+1][y] == numberIncreased:
+#         pathResult+= findPath(x+1, y, numberIncreased, positionAlreadyVisited)
+    
+#     if y > 0 and position[x][y-1] == numberIncreased:
+#         pathResult+= findPath(x, y-1, numberIncreased, positionAlreadyVisited)
+    
+#     if y < len(position[0]) - 1 and position[x][y+1] == numberIncreased:
+#         pathResult+= findPath(x, y+1, numberIncreased, positionAlreadyVisited)
+    
+#     return pathResult
+
+# result = 0
+# for x,y in startPosition:
+#     result = result + findPath(x, y, 0, set())
+
+# print(result)
 
 
 
