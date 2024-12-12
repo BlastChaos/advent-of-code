a18c7c6895c9bfac73cebd031140ff58eae01397
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-12-11 21:36:59 -0500

    day 10 part 2

--- Day 11/response.py
+++ Day 11/response.py
@@ -2,45 +2,59 @@
 inputPath = "./Day 11/input.txt"
 file = open(inputPath, "r")
 
-numbers = []
+stones = {}
 
-blinkTime = 25
+blinkTime = 75
 
 contents = file.readlines()[0].strip().split(" ")
 
 for content in contents:
-    numbers.append(int(content))
+    stones[int(content)] = 1
 
 
-print(numbers)
+print(stones)
 
-def blink(numbers):
-    newNumbers = []
-    for number in numbers:
-        stringNumber = str(number)
-        strLength = len(stringNumber)
+def blink(currentStones):
+    newStones = currentStones.copy()
+    for key, currentCount in currentStones.items():
+        newNumbers = blink_one_stone(key)
+        for number in newNumbers:
+            if number in newStones:
+                newStones[number] += currentCount
+            else:
+                newStones[number] = currentCount
+        newStones[key] += -1 * currentCount
+        if newStones[key] == 0:
+            del newStones[key]    
+    return newStones
 
-        if(number == 0):
-            newNumbers.append(1)
-            continue
 
-        if (strLength % 2 == 0):
-            value1 = stringNumber[:strLength//2]
-            value2 = stringNumber[strLength//2:]
-            newNumbers.append(int(value1))
-            newNumbers.append(int(value2))
-            continue
+def blink_one_stone(number):
+    stringNumber = str(number)
+    strLength = len(stringNumber)
 
-        newNumbers.append(number * 2024)
-    return newNumbers
+    if(number == 0):
+        return [1]
+
+    if (strLength % 2 == 0):
+        value1 = stringNumber[:strLength//2]
+        value2 = stringNumber[strLength//2:]
+        return [int(value1), int(value2)]
+    
+    return [number * 2024]
 
 
 
 
 for i in range(0, blinkTime):
-    numbers = blink(numbers)
+    stones = blink(stones)
 
-print(len(numbers))
+count = 0
+for key, value in stones.items():
+    count+=value
+print(stones)
+
+print(count)
 
 
 
